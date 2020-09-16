3f5d4f016622073530c77b3f2c10cbf679ecc793
Natalia Teaca <lilikoy@sbcglobal.net>
2020-09-16 12:09:23 -0700

    user model

--- models/User.js
+++ models/User.js
@@ -1,30 +1,54 @@
-username
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+require('mongoose-type-email');
 
-String
-Unique
-Required
-Trimmed
+const UserSchema = new Schema(
+    {
+username: {
+type: String,
+unique: true,
+required: true,
+trim: true
+},
 
-email
+email: {
+    type: String,
+    unique: true,
+required: true,
+type: mongoose.SchemaTypes.Email
+},
 
-String
-Required
-Unique
-Must match a valid email address (look into Mongoose's matching validation)
+thoughts: [
+    {
+    type: Schema.Types.ObjectId,
+    ref: 'Thought'
+    }
+],
 
+friends: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+          getters: true
+        },
+        id: false
+      }
+    );
 
-thoughts
-
-Array of _id values referencing the Thought model
-
-
-
-friends
-
-Array of _id values referencing the User model (self-reference)
-
-
-
-Schema Settings
-
-Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+// get total count of comments and replies on retrieval
+Userchema.virtual('friendCount').get(function() {
+    return this.friends.reduce((total, friends) => total + friends.length + 1, 0);
+  });
+  
+   
+    // create the Pizza model using the PizzaSchema
+  const User = model('User', UserSchema);
+  
+  // export the Pizza model
+  module.exports = User;
